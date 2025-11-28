dc94e9b489111a91d78a8d9f8d8972c600edb229
MB19-22 <martinbole2000@gmail.com>
2025-11-28 01:11:47 -0300

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,8 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+int funcion_comida(int comida);
+int funcion_refugio(int refugio);
+int funcion_balsa (int balsa);
+
+#endif // FUNCIONES_H_INCLUDED
